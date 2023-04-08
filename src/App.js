e95eef18c1d7e41e79824dd9d3c2c7935f645322
aaerciyas <aaerciyas@gmail.com>
2023-04-08 19:30:16 +0200

    change app text

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Dont Learn React
+          Feature branch
         </a>
       </header>
     </div>
