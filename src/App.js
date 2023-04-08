78c65966dad1a6f5c0e24b61f7169a0926d1171d
aaerciyas <aaerciyas@gmail.com>
2023-04-08 19:40:00 +0200

    npm fix?

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Feature branch
+          Learn React
         </a>
       </header>
     </div>
